8bfd107ba3815ac7a5f3861461b9bc9b887d5af4
Steven Isbell <steveni_92@msn.com>
2018-11-09 17:05:01 -0600

    queries

--- resolvers/Query.ts
+++ resolvers/Query.ts
@@ -1,1 +1,11 @@
-const todo = (_, { id }) => {};
+import todos from '../models/todos';
+
+const todo = (_: object, { id }: { id: number }) => {
+  return todos.find(todo => todo.id === +id);
+};
+
+const Query = {
+  todo
+};
+
+export default Query;
