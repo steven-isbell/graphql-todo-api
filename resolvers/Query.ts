7bad00ce1294061c775eb4e6bb037836ddf33be7
Steven Isbell <steveni_92@msn.com>
2018-11-13 14:10:13 -0700

    fixing type issue

--- resolvers/Query.ts
+++ resolvers/Query.ts
@@ -4,7 +4,7 @@ import { Todo } from '../types';
 // compilation fails if typing properies on objects
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/21359
 const todo = (_: object, args: any): Todo => {
-  const item = todoItems.find(todo => todo.id === args.id);
+  const item = todoItems.find(todo => todo.id === +args.id);
 
   if (!item) throw new Error(`No Item Matching ID: ${args.id}`);
   return item;
