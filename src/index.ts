e8b08a464e939932a38decf71eb6f8002f065ba5
Steven Isbell <steveni_92@msn.com>
2018-11-23 19:31:16 -0700

    adding session

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga';
+import session from 'express-session';
+import dotenv from 'dotenv';
 
 import resolvers from './resolvers';
+
+dotenv.config();
 
 const options = {
   port: 3001,
   endpoint: '/graphql',
-  playground: '/graphiql'
+  playground: '/graphiql',
+  cors: {
+    credentials: true
+  }
 };
 
 const server = new GraphQLServer({
@@ -15,6 +22,20 @@ const server = new GraphQLServer({
     ...req.request
   })
 });
+
+const secret = process.env.SESSION_SECRET || 'abcd';
+
+server.express.use(
+  session({
+    secret,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 86400000
+    }
+  })
+);
 
 server.start(options, () =>
   console.log(`Server is running on localhost:${options.port}`)
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,18 +1,37 @@
 import client from './client';
 import Todo from '../types/Todo';
 
-const getCacheValue = (key: string) =>
-  new Promise(resolve => {
+const getCacheValue = (key: string): Promise<Todo[]> =>
+  new Promise((resolve, reject) => {
     client.get(key, (err, results) => {
       if (err) {
-        resolve(false);
+        reject(false);
       } else {
         resolve(JSON.parse(results));
       }
     });
   });
 
-const addCacheValue = (key: string, value: Todo[]) =>
-  new Promise((resolve, reject) => {
-    client.setex(key, 86400, JSON.stringify(value));
+const addCacheValue = (key: string, value: Todo): Promise<Todo[]> =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const items = await getCacheValue(key);
+      items.push(value);
+      client.setex(key, 86400, JSON.stringify(items));
+      resolve(items);
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+const removeCacheValue = (key: string, id: number): Promise<Todo[]> =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const items = await getCacheValue(key);
+      let updated = items.filter(val => val.id !== id);
+      client.set(key, JSON.stringify(updated));
+      resolve(updated);
+    } catch (e) {
+      reject(e);
+    }
   });
