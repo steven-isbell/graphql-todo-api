96f762027cfc842f68e6871734ffd550c176ebe5
Steven Isbell <steveni_92@msn.com>
2019-01-16 14:18:25 -0700

    progress

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -4,7 +4,7 @@ import Todo from '../types/Todo';
 // compilation fails if typing properies on objects
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/21359
 const todo = (_: object, args: any, ctx: Context): Todo => {
-  const item = ctx.session.todos.find((todo: Todo) => todo.id === +args.id);
+  const item = ctx.session.todos.find((todo: Todo) => todo.id === args.id);
 
   if (!item) throw new Error(`No Item Matching ID: ${args.id}`);
   return item;
