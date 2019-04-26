62caa437a9cb78aabd06ae887a30777fc7eac5dd
Steven Isbell <steveni_92@msn.com>
2019-04-25 22:10:44 -0600

    clean up

--- src/connectRedisClient.ts
+++ src/connectRedisClient.ts
@@ -2,8 +2,6 @@ import redis from 'redis';
 
 const { REDIS_HOST = '', REDIS_PORT = '' } = process.env;
 
-console.log(REDIS_HOST, REDIS_PORT);
-
 const redisClient = redis.createClient({
   host: REDIS_HOST,
   port: parseInt(REDIS_PORT, 10)
